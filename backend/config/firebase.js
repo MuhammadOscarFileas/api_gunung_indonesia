684a8df4acd05dc32c953fcfd975aefc43868a9d
MuhammadOscarFileas <oscarfileasj@gmail.com>
2025-06-07 00:05:43 +0700

    debug

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,12 +1,17 @@
-import admin from 'firebase-admin';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const firebaseConfig = JSON.parse(process.env._FIREBASE_CONFIG);
+console.log('FIREBASE_CONFIG:', process.env.FIREBASE_CONFIG);
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+
+import admin from 'firebase-admin';
 
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: firebaseConfig.project_id,
     clientEmail: firebaseConfig.client_email,
-    privateKey: firebaseConfig.private_key.replace(/\\n/g, '\n') // penting
+    privateKey: firebaseConfig.private_key.replace(/\\n/g, '\n')
   })
 });
 
