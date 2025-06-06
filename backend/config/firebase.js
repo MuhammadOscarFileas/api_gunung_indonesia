14b21ae8db764779802c0e9ac6608beebad366da
MuhammadOscarFileas <oscarfileasj@gmail.com>
2025-06-07 00:27:17 +0700

    debug

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,20 +1,20 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-console.log(process.env);
-
-console.log('FIREBASE_CONFIG:', process.env._FIREBASE_CONFIG);
-
-const firebaseConfig = JSON.parse(process.env._FIREBASE_CONFIG);
-
 import admin from 'firebase-admin';
+
+const base64 = process.env.FIREBASE_CONFIG_BASE64;
+
+if (!base64) {
+  throw new Error('Missing FIREBASE_CONFIG_BASE64 environment variable');
+}
+
+const jsonString = Buffer.from(base64, 'base64').toString('utf-8');
+const firebaseConfig = JSON.parse(jsonString);
 
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: firebaseConfig.project_id,
     clientEmail: firebaseConfig.client_email,
-    privateKey: firebaseConfig.private_key.replace(/\\n/g, '\n')
-  })
+    privateKey: firebaseConfig.private_key.replace(/\\n/g, '\n'),
+  }),
 });
 
 export default admin;
